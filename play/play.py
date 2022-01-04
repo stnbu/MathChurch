d45fc793669bd3d606906e9bb21e9f8485479fad
Mike Burr <mb@unintuitive.org>
2022-01-04 13:02:49 -0700

    wee

--- play/play.py
+++ play/play.py
@@ -2,18 +2,10 @@
 
 from manim import *
 
-class ManimCELogo(Scene):
+class SomeEquasion(Scene):
     def construct(self):
-        self.camera.background_color = "#ece6e2"
-        logo_green = "#87c2a5"
-        logo_blue = "#525893"
-        logo_red = "#e07a5f"
-        logo_black = "#343434"
-        ds_m = MathTex(r"\mathbb{M}", fill_color=logo_black).scale(7)
+        ds_m = MathTex(r"E = mc^2", fill_color="#343434").scale(7)
         ds_m.shift(2.25 * LEFT + 1.5 * UP)
-        circle = Circle(color=logo_green, fill_opacity=1).shift(LEFT)
-        square = Square(color=logo_blue, fill_opacity=1).shift(UP)
-        triangle = Triangle(color=logo_red, fill_opacity=1).shift(RIGHT)
-        logo = VGroup(triangle, square, circle, ds_m)  # order matters
+        logo = VGroup(ds_m)
         logo.move_to(ORIGIN)
         self.add(logo)
