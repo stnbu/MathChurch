cdabb644098ad456efca7d3cc21d742965a140ca
Mike Burr <mburr@unintuitive.org>
2022-01-04 09:27:08 -0700

    tutorial. step three.

--- play/manim_tutorial/project/scene.py
+++ play/manim_tutorial/project/scene.py
@@ -19,3 +19,14 @@ class SquareToCircle(Scene):
         self.play(Create(square))
         self.play(Transform(square, circle))
         self.play(FadeOut(square))
+
+class SquareAndCircle(Scene):
+    def construct(self):
+        circle = Circle()
+        circle.set_fill(PINK, opacity=0.5)
+
+        square = Square()
+        square.set_fill(BLUE, opacity=0.5)
+
+        square.next_to(circle, LEFT, buff=0.5)
+        self.play(Create(circle), Create(square))
