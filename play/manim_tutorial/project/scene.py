b3f4c043be59ab21cc926cc7dab0becf1c11fe1c
Mike Burr <mburr@unintuitive.org>
2022-01-04 09:20:44 -0700

    unneeded stinking comments

--- play/manim_tutorial/project/scene.py
+++ play/manim_tutorial/project/scene.py
@@ -4,18 +4,18 @@ from manim import *
 
 class CreateCircle(Scene):
     def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
-        self.play(Create(circle))  # show the circle on screen
+        circle = Circle()
+        circle.set_fill(PINK, opacity=0.5)
+        self.play(Create(circle))
 
 class SquareToCircle(Scene):
     def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+        circle = Circle()
+        circle.set_fill(PINK, opacity=0.5)
 
-        square = Square()  # create a square
-        square.rotate(PI / 4)  # rotate a certain amount
+        square = Square()
+        square.rotate(PI / 4)
 
-        self.play(Create(square))  # animate the creation of the square
-        self.play(Transform(square, circle))  # interpolate the square into the circle
-        self.play(FadeOut(square))  # fade out animation
+        self.play(Create(square))
+        self.play(Transform(square, circle))
+        self.play(FadeOut(square))
