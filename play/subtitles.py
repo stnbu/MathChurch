94068d1a5c4e70b604b2c9fc0f02affb1635cf83
Mike Burr <mb@unintuitive.org>
2022-01-05 12:46:30 -0700

    print "subtitles" with manim "programmatically" (from within a python script)

--- play/subtitles.py
+++ play/subtitles.py
@@ -1,42 +1,89 @@
+#!/usr/bin/env python3
+
+from pathlib import PosixPath
 from manim import *
 
-class Foo(Scene):
+
+class Subtitles(Scene):
     def construct(self):
-        ellipse1 = Ellipse(
-            width=4.0, height=5.0, fill_opacity=0.5, color=BLUE, stroke_width=10
-        ).move_to(LEFT)
-        ellipse2 = ellipse1.copy().set_color(color=RED).move_to(RIGHT)
-        bool_ops_text = MarkupText("<u>Boolean Operation</u>").next_to(ellipse1, UP * 3)
-        ellipse_group = Group(bool_ops_text, ellipse1, ellipse2).move_to(LEFT * 3)
-        self.play(FadeIn(ellipse_group))
-
-
-        # So-called "subtitles"
         subs = Text("People of Earth!!")
-        subs_group = Group(subs).move_to(DOWN * 2)
-        self.play(FadeIn(subs_group))
-        
+        self.add(subs)
+        self.wait(10)
 
-        
-        i = Intersection(ellipse1, ellipse2, color=GREEN, fill_opacity=0.5)
-        self.play(i.animate.scale(0.25).move_to(RIGHT * 5 + UP * 2.5))
-        intersection_text = Text("Intersection", font_size=23).next_to(i, UP)
-        self.play(FadeIn(intersection_text))
 
-        u = Union(ellipse1, ellipse2, color=ORANGE, fill_opacity=0.5)
-        union_text = Text("Union", font_size=23)
-        self.play(u.animate.scale(0.3).next_to(i, DOWN, buff=union_text.height * 3))
-        union_text.next_to(u, UP)
-        self.play(FadeIn(union_text))
+class ClickArgs:
+    def __init__(self, args):
+        for name in args:
+            setattr(self, name, args[name])
 
-        e = Exclusion(ellipse1, ellipse2, color=YELLOW, fill_opacity=0.5)
-        exclusion_text = Text("Exclusion", font_size=23)
-        self.play(e.animate.scale(0.3).next_to(u, DOWN, buff=exclusion_text.height * 3.5))
-        exclusion_text.next_to(e, UP)
-        self.play(FadeIn(exclusion_text))
+    def _get_kwargs(self):
+        return list(self.__dict__.items())
 
-        d = Difference(ellipse1, ellipse2, color=PINK, fill_opacity=0.5)
-        difference_text = Text("Difference", font_size=23)
-        self.play(d.animate.scale(0.3).next_to(u, LEFT, buff=difference_text.height * 3.5))
-        difference_text.next_to(d, UP)
-        self.play(FadeIn(difference_text))
+    def __eq__(self, other):
+        if not isinstance(other, ClickArgs):
+            return NotImplemented
+        return vars(self) == vars(other)
+
+    def __contains__(self, key):
+        return key in self.__dict__
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+
+def do_config():
+    args = {
+        "preview": True,
+        "quality": "l",
+        "file": PosixPath("play/subtitles.py"),
+        "scene_names": ("Subtitles",),
+        "config_file": None,
+        "custom_folders": None,
+        "disable_caching": None,
+        "flush_cache": None,
+        "tex_template": None,
+        "verbosity": None,
+        "notify_outdated_version": None,
+        "enable_gui": None,
+        "gui_location": None,
+        "fullscreen": None,
+        "enable_wireframe": None,
+        "force_window": False,
+        "dry_run": False,
+        "output_file": None,
+        "zero_pad": None,
+        "write_to_movie": None,
+        "media_dir": None,
+        "log_dir": None,
+        "log_to_file": None,
+        "from_animation_number": None,
+        "write_all": None,
+        "format": None,
+        "save_last_frame": None,
+        "resolution": None,
+        "frame_rate": None,
+        "renderer": None,
+        "use_opengl_renderer": None,
+        "use_webgl_renderer": None,
+        "webgl_renderer_path": None,
+        "save_pngs": None,
+        "save_as_gif": None,
+        "save_sections": None,
+        "transparent": False,
+        "use_projection_fill_shaders": None,
+        "use_projection_stroke_shaders": None,
+        "progress_bar": None,
+        "show_in_file_browser": None,
+        "jupyter": None,
+    }
+    click_args = ClickArgs(args)
+    config.digest_args(click_args)
+
+
+if __name__ == "__main__":
+    do_config()
+    scene = Subtitles()
+    import ipdb
+
+    ipdb.set_trace()
+    scene.render()
