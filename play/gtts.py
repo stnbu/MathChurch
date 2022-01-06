6b7abe488b9962bc6ba2ad28455977373a6b4545
Mike Burr <mb@unintuitive.org>
2022-01-06 08:38:38 -0700

    Black Edition

--- play/gtts.py
+++ play/gtts.py
@@ -13,9 +13,7 @@ voice = texttospeech.VoiceSelectionParams(
     language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
 )
 
-audio_config = texttospeech.AudioConfig(
-    audio_encoding=texttospeech.AudioEncoding.MP3
-)
+audio_config = texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding.MP3)
 
 response = client.synthesize_speech(
     input=synthesis_input, voice=voice, audio_config=audio_config
