1aab826fd6a2441ac54c362a8c9ec34f3dd37a53
Mike Burr <mb@unintuitive.org>
2022-01-06 11:05:51 -0700

    we need the path I guess.

--- play/gtts.py
+++ play/gtts.py
@@ -50,13 +50,11 @@ def get_audio_bytes(input, bytes_getter):
     hash = get_normalized_input_hash(input)
     path = get_audio_cache_path(hash)
     if os.path.exists(path):
-        with open(path, "rb") as f:
-            return f.read()  # um
-
+        return open(path, "rb").read(), path
     audio_bytes = bytes_getter(input)
     with open(path, "wb") as f:
         f.write(audio_bytes)
-    return audio_bytes
+    return audio_bytes, path
 
 
 if __name__ == "__main__":
@@ -71,10 +69,11 @@ if __name__ == "__main__":
     def _exceptional_bytes_get(input):
         raise AssertionError
 
-    bytes1 = get_audio_bytes("hi planet", _test_bytes_getter)
-    bytes2 = get_audio_bytes("hi planet!!", _exceptional_bytes_get)
+    bytes1, path1 = get_audio_bytes("hi planet", _test_bytes_getter)
+    bytes2, path2 = get_audio_bytes("hi planet!!", _exceptional_bytes_get)
 
     assert bytes1 == bytes2
+    assert path1 == path2
 
     # # This works, but it will consume API resources (your money).
     # text = "Hello, World!"
