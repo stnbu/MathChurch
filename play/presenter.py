4d222ee17475f2a1f9c2fade8a1a6022473cb2df
Mike Burr <mb@unintuitive.org>
2022-01-07 09:11:51 -0700

    more-proper-module-like

--- play/presenter.py
+++ play/presenter.py
@@ -56,36 +56,37 @@ def play_lecture(lecture):
         else:
             raise ValueError
 
+if __name__ == "__main__":
 
-impressive_equation = MathTex(r"h(u*v)=h(u) \odot h(v)")
+    impressive_equation = MathTex(r"h(u*v)=h(u) \odot h(v)")
 
-lecture = [
-    """This is the first chunk of text. Let me try to explain the idea here.
-This text appears inside of a Python list. Don't worry, it's just a proof
-of concept!
-    """,
-    """The words that you are seeing now are the next chunk of subtitles
-in the demo. No biggie. Just showing you how to chunk your subs.
-    """,
-    """Now, the next exciting thing is that these subtitles are interleaved
-with "commands". I can tell Manim to display some impressive math equation.
-    """,
-    """Ready?
-    """,
-    """Here goes...
-    """,
-    ["add", impressive_equation],
-    """Can you see it? Good. That's the idea.
-    """,
-    """Now watch me make it disappear.
-    """,
-    ["remove", impressive_equation],
-    """All gone? Good. It should be!
-    """,
-    """Amen!
-    """,
-]
+    lecture = [
+        """This is the first chunk of text. Let me try to explain the idea here.
+        This text appears inside of a Python list. Don't worry, it's just a proof
+        of concept!
+        """,
+        """The words that you are seeing now are the next chunk of subtitles
+        in the demo. No biggie. Just showing you how to chunk your subs.
+        """,
+        """Now, the next exciting thing is that these subtitles are interleaved
+        with "commands". I can tell Manim to display some impressive math equation.
+        """,
+        """Ready?
+        """,
+        """Here goes...
+        """,
+        ["add", impressive_equation],
+        """Can you see it? Good. That's the idea.
+        """,
+        """Now watch me make it disappear.
+        """,
+        ["remove", impressive_equation],
+        """All gone? Good. It should be!
+        """,
+        """Amen!
+        """,
+    ]
 
-scene = Scene()
-play_lecture(lecture)
-scene.render()
+    scene = Scene()
+    play_lecture(lecture)
+    scene.render()
