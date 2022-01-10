d73772fbedb8efeb6216560ddc3b34840a1ef952
Mike Burr <mb@unintuitive.org>
2022-01-10 11:22:15 -0700

    .

--- presenter/test_tts.py
+++ presenter/test_tts.py
@@ -1,3 +1,12 @@
+"""NOTE:
+
+These test may:
+* Write to disk
+* Execute subprocesses
+* Use paid API resources (e.g. GCS)
+"""
+
+import os
 from presenter.tts import *
 
 import unittest
@@ -8,3 +17,4 @@ class TTSTests(unittest.TestCase):
         got = get_audio_path("Hello, world!", "test_engine", "tstfmt")
         expected = "media/audio/test_engine/8552d8b7a7dc5476cb9e25dee69a8091290764b7f2a64fe6e78e9568.tstfmt"
         assert got == expected
+        assert os.path.exists(os.path.dirname(got))
