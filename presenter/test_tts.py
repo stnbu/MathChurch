431b0f15268c76e272ead5c1dc3d3ec7c7f7706e
Mike Burr <mb@unintuitive.org>
2022-01-10 11:09:21 -0700

    a test

---
+++ presenter/test_tts.py
@@ -0,0 +1,10 @@
+from presenter.tts import *
+
+import unittest
+
+
+class TTSTests(unittest.TestCase):
+    def test_get_audio_path_signature(self):
+        got = get_audio_path("Hello, world!", "test_engine", "tstfmt")
+        expected = "media/audio/test_engine/8552d8b7a7dc5476cb9e25dee69a8091290764b7f2a64fe6e78e9568.tstfmt"
+        assert got == expected
