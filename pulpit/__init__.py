3202032f7d4c968306a67aea234a0253cf64295e
Mike Burr <mb@unintuitive.org>
2022-01-11 11:23:02 -0700

    subrips work

--- pulpit/__init__.py
+++ pulpit/__init__.py
@@ -11,14 +11,16 @@ the presentation (or whoever wants to).
 
 from types import FunctionType
 from .tts import *
+from .subrip import *
 from manim import *
-
 
 class Player:
     def __init__(self, scene, lecture, tts_engine):
         self.scene = scene
         self.lecture = lecture
         self.tts_engine = tts_engine
+        self.subrip_file = SubRipFile()
+        self.offset = 0.0
 
     def play(self, count=None):
         for index, item in enumerate(self.lecture):
@@ -34,17 +36,22 @@ class Player:
                     "text of length %s characters." % (path, length, len(item))
                 )
                 self.scene.add_sound(path)
-                wait = length + 0.2
-                subtitle = Text(item)
-                subtitle.scale(0.5)
-                subtitle.to_edge(DOWN)
-                self.scene.add(subtitle)
-                logger.info(
-                    "We will be adding a pause to the video of %s seconds" % wait
-                )
-                self.scene.wait(wait)
-                self.scene.remove(subtitle)
+
+                disappear = self.offset + length
+                self.subrip_file.add(SubRipChunk(
+                    appear=self.offset,
+                    disappear=disappear,
+                    text=item
+                ))
+                self.offset = disappear
+
+                self.scene.wait(length)
+
+
             elif isinstance(item, FunctionType):
                 item(self.scene)
             else:
                 raise ValueError
+
+        subrip_file_path = os.path.splitext(self.scene.renderer.file_writer.movie_file_path)[0] + '.srt'
+        self.subrip_file.write_to(subrip_file_path)
