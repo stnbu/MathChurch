b13341ac47656582d11aedb283c537c0a01928e5
Mike Burr <mb@unintuitive.org>
2022-01-13 09:03:43 -0700

    black

--- tbob/__init__.py
+++ tbob/__init__.py
@@ -9,22 +9,26 @@ from git import Repo
 
 TBOB_DIR = None
 
+
 def init():
     global TBOB_DIR
-    TBOB_DIR = os.path.expanduser('~/tbob')
+    TBOB_DIR = os.path.expanduser("~/tbob")
     os.makedirs(TBOB_DIR, exist_ok=True)
 
+
 def clean(path):
-    """The missing `repo.clean`
-    """
-    proc = subprocess.Popen('git clean -qfdx'.split(),
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE,
-                            cwd=path)
+    """The missing `repo.clean`"""
+    proc = subprocess.Popen(
+        "git clean -qfdx".split(),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        cwd=path,
+    )
     out, err = proc.communicate()
     assert len(out) == 0
     assert len(out) == 0
     assert proc.returncode == 0
+
 
 def get_repo(path, origin=None):
     """Returns a `Repo` instance corresponding to `path`. If `path` does not
@@ -34,8 +38,7 @@ def get_repo(path, origin=None):
     repo = None
     if not os.path.exists(path):
         if origin is None:
-            raise Exception("%s does not exist and no remote URL was supplied" %
-                             path)
+            raise Exception("%s does not exist and no remote URL was supplied" % path)
         repo = Repo.clone_from(url=origin, to_path=path)
     else:
         clean(path)
@@ -43,9 +46,9 @@ def get_repo(path, origin=None):
         repo.remote().pull()
     return repo
 
+
 def get_last_commit(repo, path=None):
-    """Gets the last (most recent) commit to `repo`, modulo `path` if supplied.
-    """
+    """Gets the last (most recent) commit to `repo`, modulo `path` if supplied."""
     kwargs = {}
     if path is not None:
         kwargs = {"paths": path}
@@ -53,6 +56,7 @@ def get_last_commit(repo, path=None):
         return c
     else:
         raise Exception("No commits found")
+
 
 def get_commit_before(repo, commit):
     """Going backwards through time, get the first commit from `repo` that
@@ -62,21 +66,21 @@ def get_commit_before(repo, commit):
         if c.authored_date < commit.authored_date:
             return c
     else:
-        raise Exception("No commit occurs before %s (@%d)" %
-                        (commit, commit.authored_date))
+        raise Exception(
+            "No commit occurs before %s (@%d)" % (commit, commit.authored_date)
+        )
+
 
 if __name__ == "__main__":
 
     init()
 
-    videos_repo_path = os.path.join(TBOB_DIR, '3b1b_videos')
-    manim_repo_path = os.path.join(TBOB_DIR, '3b1b_manim')
-    manim_venv_path = os.path.join(TBOB_DIR, '3b1b_venv')
+    videos_repo_path = os.path.join(TBOB_DIR, "3b1b_videos")
+    manim_repo_path = os.path.join(TBOB_DIR, "3b1b_manim")
+    manim_venv_path = os.path.join(TBOB_DIR, "3b1b_venv")
 
-    videos_repo = get_repo(videos_repo_path,
-                           "https://github.com/3b1b/videos.git")
-    manim_repo = get_repo(manim_repo_path,
-                          "https://github.com/3b1b/manim.git")
+    videos_repo = get_repo(videos_repo_path, "https://github.com/3b1b/videos.git")
+    manim_repo = get_repo(manim_repo_path, "https://github.com/3b1b/manim.git")
 
     videos_commit = get_last_commit(videos_repo, "_2021/newton_fractal.py")
     manim_commit = get_commit_before(repo, videos_commit)
