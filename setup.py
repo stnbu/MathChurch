6a7adf27740343de6e4749ccbfccb87b2aaf0f0a
Mike Burr <mb@unintuitive.org>
2022-01-14 10:20:26 -0700

    better tbob

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup
 
 setup(
     name="MathChurch",
-    version="0.0.8",
+    version="0.0.9",
     description="Subs-driven presentation, with TTS audio (and more)",
     author="Mike Burr",
     author_email="mb+church@unintuitive.org",
     url="https://github.com/stnbu/MathChurch",
     packages=["pulpit"],
-    scripts=["tbob/run.sh"],
+    scripts=["tbob/tbob"],
     entry_points={"console_scripts": ["_tbob_helper = tbob.__main__:main"]},
 )
