525d480cbee509545b3f7f7458a1b3198caa997e
Mike Burr <mb@unintuitive.org>
2022-01-19 13:54:26 -0700

    we can calculate the total animation time just _once_.

--- pulpit/__init__.py
+++ pulpit/__init__.py
@@ -16,22 +16,6 @@ from manim import *
 
 SubChunk = namedtuple("SubChunk", ["text", "actions"], defaults=[[]])
 
-def get_total_waits(actions):
-    # FIXME: This is B.A.D.
-    # Can we get around it? We need to find actions' total duration
-    # before we can know how much wait to add for the audio clip to
-    # finish, BUT we must, in order...
-    #   1. play audio
-    #   1. play animations
-    #   1. wait for "audio length MINUS ANIMATIONS LENGTH"
-    # I know of no other way to calculate actions' duration. There's
-    # a short-circuit, see.
-    s = Scene()
-    start_time = s.renderer.time
-    for action in actions:
-        action(s)
-    return s.renderer.time - start_time
-
 class Player:
     def __init__(self, scene, lecture, tts_engine, baked=False, ripped=True):
         self.scene = scene
@@ -45,7 +29,6 @@ class Player:
         sound_track_offset = 0.0
         waiting_until = 0.0
         for chunk in self.lecture:
-            actions_wait = get_total_waits(chunk.actions)
             path, length = self.tts_engine(chunk.text)
             self.scene.add_sound(path)
 
@@ -54,6 +37,11 @@ class Player:
                 subtitle.scale(0.5)
                 subtitle.to_edge(DOWN)
                 self.scene.add(subtitle)
+
+            start_time = self.scene.renderer.time
+            for action in chunk.actions:
+                action(self.scene)
+            actions_wait = self.scene.renderer.time - start_time
 
             if actions_wait < length:
                 self.scene.wait(length - actions_wait)
@@ -68,12 +56,8 @@ class Player:
                         text=chunk.text,
                     )
                 )
+
             sound_track_offset = sound_track_offset + length
-
-            for action in chunk.actions:
-                action(self.scene)
-
-
 
         subrip_file_path = (
             os.path.splitext(self.scene.renderer.file_writer.movie_file_path)[0]
