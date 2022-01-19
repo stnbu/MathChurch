9fde20e33ab9815f2cbb6f6fd604c9778c1c73d0
Mike Burr <mb@unintuitive.org>
2022-01-19 15:23:21 -0700

    manually copy 0_neg_num.py changes to main

--- liturgy/0_negative_numbers.py
+++ liturgy/0_negative_numbers.py
@@ -1,6 +1,13 @@
+import os
 from pulpit import *
 
-pills = MathTex(r"hand.holding(red_pill, blue_pill)")
+
+def asset(filename):
+    return os.path.join(os.path.splitext(__file__)[0] + ".assets", filename)
+
+
+pills = ImageMobject(asset("pills.jpg"))
+pills.scale(0.5)
 negative_three = MathTex(r"-3")
 animated_numbers = MathTex(r"flashing(1...2...3...)")
 flashing_plus = MathTex(r"5 flash(+) 2")
@@ -11,138 +18,76 @@ add_apples = MathTex(r"apple + apple")
 subtract_apples = MathTex(r"apple - apple")
 
 lecture = [
-    """What <emphasis level="strong">are</emphasis> negative numbers?
-    """,
+    SubChunk("""What are negative numbers?"""),
 
-    """You likely learned about negative numbers long ago. Maybe they have lost
-       their mystery for you.
-    """,
+    SubChunk("""You likely learned about negative numbers long ago. Maybe they have lost
+       their mystery for you."""),
 
-    lambda scene: scene.add(pills),
-
-    """But what if I told you there's more here than you think? There are even
+    SubChunk("""But what if I told you there's more here than you think. There are even
        some hints about what professional mathematitions actually do in something
-       as mundane as negative numbers.
-    """,
+       as mundane as negative numbers.""",
+             actions=[
+                 lambda scene: scene.play(FadeIn(pills), run_time=3),
+                 lambda scene: scene.play(FadeOut(pills), run_time=3)
+             ]),
 
-    lambda scene: scene.remove(pills),
-
-    lambda scene: scene.add(negative_three),
-
-    """That hoizontal stroke you see to the left of a number is a symbol. It has
+    SubChunk("""That hoizontal stroke you see to the left of a number is a symbol. It has
        meaning. Just as the symbol you see on the right has meaning: it's a
-       number.
-    """,
-    
-    lambda scene: scene.remove(negative_three),
+       number.""",
+             actions=[lambda scene: scene.add(negative_three)]),
 
-    lambda scene: scene.add(animated_numbers),
-
-    """But maybe you got so used to seeing these symobols that you lost
-       your appriciation for their _real_ meaning. What _is_ three? Give it a
+    SubChunk("""But maybe you got so used to seeing these symobols that you lost
+       your appriciation for their real meaning. What is three? Give it a
        long think sometime. You might find it to be more profound than you gave
-       credit in the past.
-    """,
+       credit in the past.""",
+             actions=[lambda scene: scene.remove(negative_three),
+                      lambda scene: scene.add(animated_numbers)
+                      ]),
 
-    lambda scene: scene.remove(animated_numbers),
-
-    """Likewise, that "dash" that you see to the left of a "negative number" is
-       also just a symbol. Why is it a dash? Why not three dots? Why not a
-       bunny?
-    """,
-
-    """Someone had to invent that symbol, and we ended up with a kind of "dash".
-       This same symbol is used as an _operator_.
-    """,
-
-    lambda scene: scene.add(flashing_plus),
-
-    """Just as we use the plus symbol as an operator...
-    """,
-
-    lambda scene: scene.remove(flashing_plus),
-
-    lambda scene: scene.add(flashing_minus),
-
-    """So can we use the minus symbol as an operator.
-    """,
-
-    lambda scene: scene.remove(flashing_minus),
-
-    """But let's dewell on the "symbol" aspect for just a moment.
-    """,
-
-    lambda scene: scene.add(bare_integral),
-
-    """Do you know what this symbol means?
-    """,
-
-    """You may have no idea, or if you've studied calculus you may get excited
-       by this symbol.
-    """,
-
-    """Rest assured: It's just a symbol.
-    """,
-
-    lambda scene: scene.remove(bare_integral),
-
-    """As with all symbols, the important question is, "What does it _mean_?"
-    """,
-
-    """You can think of the minus sign as meaning "has debt" as an approximation.
-    """,
-
-    """Likewise, you can think of the plus sign as meaning, "has credit".
-    """,
-
-    """Let's use black to represent credit and red to represent debt.
-    """,
-
-    """And as is standard with these things, let's an apple to denote "a thing".
-    """,
-
-    lambda scene: scene.add(add_apples),
-
-    """I bet you know how many apples we have after having done this!
-    """,
-
-    lambda scene: scene.remove(add_apples),
-
-    """But now, if I show you an expression with _debt apples_...
-    """,
-
-    lambda scene: scene.add(subtract_apples),
-
-    """
-    """,
-
-    """
-    """,
-
-    """
-    """,
-
-    """
-    """,
-
-    """
-    """,
-
-    """
-    """,
-
-    """
-    """,
-
-    """
-    """,
-
-    """
-    """,
-
-    """
-    """,
-
+    # lambda scene: scene.remove(animated_numbers),
+    # SubChunk("""Likewise, that "dash" that you see to the left of a "negative number" is
+    #    also just a symbol. Why is it a dash? Why not three dots? Why not a
+    #    bunny?
+    # """),
+    # SubChunk("""Someone had to invent that symbol, and we ended up with a kind of "dash".
+    #    This same symbol is used as an operator.
+    # """),
+    # lambda scene: scene.add(flashing_plus),
+    # SubChunk("""Just as we use the plus symbol as an operator...
+    # """),
+    # lambda scene: scene.remove(flashing_plus),
+    # lambda scene: scene.add(flashing_minus),
+    # SubChunk("""So can we use the minus symbol as an operator.
+    # """),
+    # lambda scene: scene.remove(flashing_minus),
+    # SubChunk("""But let's dewell on the "symbol" aspect for just a moment.
+    # """),
+    # lambda scene: scene.add(bare_integral),
+    # SubChunk("""Do you know what this symbol means?
+    # """),
+    # SubChunk("""You may have no idea, or if you've studied calculus you may get excited
+    #    by this symbol.
+    # """),
+    # SubChunk("""Rest assured: It's just a symbol.
+    # """),
+    # lambda scene: scene.remove(bare_integral),
+    # SubChunk("""As with all symbols, the important question is, "What does it mean?"
+    # """),
+    # SubChunk("""You can think of the minus sign as meaning "has debt" as an approximation.
+    # """),
+    # SubChunk("""Likewise, you can think of the plus sign as meaning, "has credit".
+    # """),
+    # SubChunk("""Let's use black to represent credit and red to represent debt.
+    # """),
+    # SubChunk("""And as is standard with these things, let's an apple to denote "a thing".
+    # """),
+    # lambda scene: scene.add(add_apples),
+    # SubChunk("""I bet you know how many apples we have after having done this!
+    # """),
+    # lambda scene: scene.remove(add_apples),
+    # SubChunk("""But now, if I show you an expression with debt apples...
+    # """),
+    # lambda scene: scene.add(subtract_apples),
 ]
 
 scene = Scene()
